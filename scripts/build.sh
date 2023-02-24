d70bdd3e7f1e76af7b65d89da62a1afa718c3114
Hongyu <hongyuhe.cs@googlemail.com>
2023-02-24 18:49:46 +0100

    Update setup scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+set -x
+set -e
 
 EXAMPLE=$1
+
 cd hw && mkdir build && cd build || exit
 /bin/cmake .. -DFDEV_NAME=u55c -DEXAMPLE="$EXAMPLE"
+make shell && make compile
 
---
+++ scripts/load_env.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+source /opt/Xilinx/Vivado/2022.2/settings64.sh
+export XILINXD_LICENSE_FILE='2100@sgv-license-01.ethz.ch'
